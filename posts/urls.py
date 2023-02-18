e2fc5dde427a51112327a6866fcb77b1fddfb93b
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-18 08:50:07 -0800

    Add posts to board

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
 urlpatterns = [
+    path("newpost/", PostCreateView.as_view(), name="new-post"),
     path("", PostListView.as_view(), name="home")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
 class PostListView(ListView):
     model = Post
     template_name = "home.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "new_post.html"
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)

9c7202d75167787b417e5b499fdf089fd6a59aff
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-18 09:24:49 -0800

    Add member attribute to accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = [
+        "username",
+        "member",
+        "is_staff"
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("member",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("member",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user",null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user",null=True)
     body=models.TextField()
 
     def __str__(self):

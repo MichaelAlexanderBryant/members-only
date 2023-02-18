8a34628157a2e0e3e8e8c712b62e7f49eec6790d
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-17 19:29:52 -0800

    Add member code html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, MemberCodeView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("membercode/", MemberCodeView.as_view(), name="member-code")
 ]
